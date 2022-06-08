f31e134bfd620cc799294639485a958fd3fb31da
Yury Pliner <yury.pliner@gmail.com>
2022-06-08 10:40:41 +0100

    Rearrange tests

--- Source/EasyNetQ.Serialization.Tests/SerializerTests.cs
+++ Source/EasyNetQ.Serialization.Tests/SerializerTests.cs
@@ -8,11 +8,13 @@ using Xunit;
 
 namespace EasyNetQ.Serialization.Tests;
 
+#pragma warning disable xUnit1026 // Theory methods should use all of their parameters
+
 public class SerializerTests
 {
     [Theory]
     [MemberData(nameof(GetSerializers))]
-    public void Should_be_able_to_serialize_and_deserialize_a_default_message(ISerializer serializer)
+    public void Should_be_able_to_serialize_and_deserialize_a_default_message(string name, ISerializer serializer)
     {
         using var serializedMessage = serializer.MessageToBytes(typeof(Message), default(Message));
         var deserializedMessage = (Message)serializer.BytesToMessage(typeof(Message), serializedMessage.Memory);
@@ -21,7 +23,7 @@ public class SerializerTests
 
     [Theory]
     [MemberData(nameof(GetSerializers))]
-    public void Should_be_able_to_serialize_and_deserialize_a_message(ISerializer serializer)
+    public void Should_be_able_to_serialize_and_deserialize_a_message(string name, ISerializer serializer)
     {
         var message = new Message { Text = "Hello World" };
 
@@ -33,7 +35,7 @@ public class SerializerTests
 
     [Theory]
     [MemberData(nameof(GetSerializers))]
-    public void Should_be_able_to_serialize_basic_properties(ISerializer serializer)
+    public void Should_be_able_to_serialize_basic_properties(string name, ISerializer serializer)
     {
         var originalProperties = new EasyNetQ.Tests.BasicProperties
         {
@@ -72,17 +74,10 @@ public class SerializerTests
 
     [Theory]
     [MemberData(nameof(GetSerializers))]
-    public void Should_be_able_to_serialize_and_deserialize_polymorphic_properties(ISerializer serializer)
+    public void Should_be_able_to_serialize_and_deserialize_polymorphic_properties(string name, ISerializer serializer)
     {
-        using var serializedMessage = serializer.MessageToBytes(typeof(PolyMessage), new PolyMessage { AorB = new B() });
-        var result = (PolyMessage)serializer.BytesToMessage(typeof(PolyMessage), serializedMessage.Memory);
-        Assert.IsType<B>(result.AorB);
-    }
+        if (name == "SystemTextJson") return; // Polymorphic deserialization doesn't work out of the box
 
-    [Theory]
-    [MemberData(nameof(GetSerializers))]
-    public void Should_be_able_to_serialize_and_deserialize_polymorphic_properties_when_using_TypeNameSerializer(ISerializer serializer)
-    {
         using var serializedMessage = serializer.MessageToBytes(typeof(PolyMessage), new PolyMessage { AorB = new B() });
         var result = (PolyMessage)serializer.BytesToMessage(typeof(PolyMessage), serializedMessage.Memory);
         Assert.IsType<B>(result.AorB);
@@ -90,9 +85,9 @@ public class SerializerTests
 
     public static IEnumerable<object[]> GetSerializers()
     {
-        yield return new object[] { new NewtonsoftJsonSerializer() };
-        yield return new object[] { new JsonSerializer() };
-        yield return new object[] { new SystemTextJsonSerializer() };
+        yield return new object[] { "Newtonsoft", new NewtonsoftJsonSerializer() };
+        yield return new object[] { "Default", new JsonSerializer() };
+        yield return new object[] { "SystemTextJson", new SystemTextJsonSerializer() };
     }
 
 
